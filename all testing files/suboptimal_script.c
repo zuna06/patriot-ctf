ca312a6c9673e58d43bf212a0c3b36cea248a9ef
zuna06 <tanishambala94@gmail.com>
2023-09-08 22:14:26 -0400

    golmol

---
+++ all testing files/suboptimal_script.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    char key_stuff[] = "xk|nF{quxzwkgzgwx|quitH";
+    long int arr[25];
+    for (int i = 0; i < sizeof(key_stuff); i++) {
+        //arr[i] = (int) key_stuff[i];
+        printf("%i\n", key_stuff[i]);
+        
+    }
+
+
+
+}

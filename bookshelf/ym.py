5ae04dd83b143a846186a29a53da7e8acce01db7
Cherry <cherry@sparklet.org>
2023-09-09 00:48:38 -0400

    read bot command txt file for bookshelf

--- bookshelf/ym.py
+++ bookshelf/ym.py
@@ -1,37 +1,32 @@
 import socket
 from time import sleep
 
-# try "Giraffe"
+SLEEP_LEN = 0.5
 
-SLEEP_LEN = 0.1
+with open("steps.txt", "r") as file:
+    INIT_PROMPT = file.read()
 
-with open("animals.txt", "r") as file:
-    for line in file:
-        try:
-            # Create a socket and connect to the server
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect(("chal.pctf.competitivecyber.club", 9999))
 
-            # Send the current line to the server
-            client_socket.send(line.lower().encode())
+def connect():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(("chal.pctf.competitivecyber.club", 4444))
 
-            sleep(SLEEP_LEN)
+    return client_socket
 
-            # get the second line
-            full_response = client_socket.recv(4096)
-            response = full_response.decode().split("\n")[1]
 
-            sus = "ERRR! Wrong!" not in response
-            sus_fmt = "SUS!" if sus else "fail:"
+try:
+    conn = connect()
 
-            print(f"{sus_fmt} {line}")
-            if sus:
-                print(f"Full Response: {full_response}")
+    # Send the current line to the server
+    conn.send(INIT_PROMPT.encode())
+    sleep(SLEEP_LEN)
 
-            # Close the socket
-            client_socket.close()
+    # get the second line
+    full_response = conn.recv(4096)
 
-        except Exception as e:
-            print(f"Error: {e}")
+    print(full_response.decode())
 
-print("Script completed.")
+    # Close the socket
+    conn.close()
+except Exception as e:
+    print(f"Error: {e}")
